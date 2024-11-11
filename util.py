422875160c1dff3d34ac5c9ee016d14c3971c093
Feziwe Melvin <melvin.shongwe@gmail.com>
2024-11-11 07:36:25 +0200

    Update util.py

--- util.py
+++ util.py
@@ -1,27 +1,16 @@
-import sys
 import copy
 import random
 import numpy as np
 from collections import defaultdict
 
-
-def data_partition(fname):
-    usernum = 0
-    itemnum = 0
-    User = defaultdict(list)
+def data_partition(user_data):
     user_train = {}
     user_valid = {}
     user_test = {}
 
-    # Using 'with' for better file management
-    with open(f'SASRec/data/{fname}.txt', 'r') as f:
-        for line in f:
-            u, i = line.rstrip().split(' ')
-            u = int(u)
-            i = int(i)
-            usernum = max(u, usernum)
-            itemnum = max(i, itemnum)
-            User[u].append(i)
+    User = defaultdict(list)
+    for _, row in user_data.iterrows():
+        User[row['name']].append(row['no_of_ratings'])
 
     for user in User:
         nfeedback = len(User[user])
@@ -34,21 +23,13 @@ def data_partition(fname):
             user_valid[user] = [User[user][-2]]
             user_test[user] = [User[user][-1]]
 
-    return [user_train, user_valid, user_test, usernum, itemnum]
-
+    return [user_train, user_valid, user_test, len(User), len(User[user])]
 
 def evaluate(model, dataset, args, sess):
     [train, valid, test, usernum, itemnum] = copy.deepcopy(dataset)
+    NDCG, HT, valid_user = 0.0, 0.0, 0.0
 
-    NDCG = 0.0
-    HT = 0.0
-    valid_user = 0.0
-
-    # Using range instead of xrange
-    if usernum > 10000:
-        users = random.sample(range(1, usernum + 1), 10000)
-    else:
-        users = range(1, usernum + 1)
+    users = random.sample(range(1, usernum + 1), 10000) if usernum > 10000 else range(1, usernum + 1)
     
     for u in users:
         if len(train[u]) < 1 or len(test[u]) < 1:
@@ -64,72 +45,11 @@ def evaluate(model, dataset, args, sess):
             if idx == -1:
                 break
         
-        rated = set(train[u])
-        rated.add(0)
-        item_idx = [test[u][0]]
-        for _ in range(100):
-            t = np.random.randint(1, itemnum + 1)
-            while t in rated:
-                t = np.random.randint(1, itemnum + 1)
-            item_idx.append(t)
-
-        predictions = -model.predict(sess, [u], [seq], item_idx)
-        predictions = predictions[0]
-
+        item_idx = [test[u][0]] + [random.randint(1, itemnum + 1) for _ in range(100)]
+        predictions = -model.predict(sess, [u], [seq], item_idx)[0]
         rank = predictions.argsort().argsort()[0]
 
         valid_user += 1
-
-        if rank < 10:
-            NDCG += 1 / np.log2(rank + 2)
-            HT += 1
-        if valid_user % 100 == 0:
-            print('.', end='', flush=True)
-
-    return NDCG / valid_user, HT / valid_user
-
-
-def evaluate_valid(model, dataset, args, sess):
-    [train, valid, test, usernum, itemnum] = copy.deepcopy(dataset)
-
-    NDCG = 0.0
-    valid_user = 0.0
-    HT = 0.0
-
-    # Using range instead of xrange
-    if usernum > 10000:
-        users = random.sample(range(1, usernum + 1), 10000)
-    else:
-        users = range(1, usernum + 1)
-
-    for u in users:
-        if len(train[u]) < 1 or len(valid[u]) < 1:
-            continue
-
-        seq = np.zeros([args.maxlen], dtype=np.int32)
-        idx = args.maxlen - 1
-        for i in reversed(train[u]):
-            seq[idx] = i
-            idx -= 1
-            if idx == -1:
-                break
-
-        rated = set(train[u])
-        rated.add(0)
-        item_idx = [valid[u][0]]
-        for _ in range(100):
-            t = np.random.randint(1, itemnum + 1)
-            while t in rated:
-                t = np.random.randint(1, itemnum + 1)
-            item_idx.append(t)
-
-        predictions = -model.predict(sess, [u], [seq], item_idx)
-        predictions = predictions[0]
-
-        rank = predictions.argsort().argsort()[0]
-
-        valid_user += 1
-
         if rank < 10:
             NDCG += 1 / np.log2(rank + 2)
             HT += 1
